f4d762c8eba463932b09f2890ec5929f8c30d279
Bedevi Hadaroglu <ayipsinvesselam@gmail.com>
2024-06-10 21:17:52 +0300

    Update functions.dart

--- lib/functions.dart
+++ lib/functions.dart
@@ -2,4 +2,4 @@ import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 
 CallNumber(String number) async {
   await FlutterPhoneDirectCaller.callNumber(number);
-} //burası 112 arayan fonksiyon kodu
+} //burası 112'yi arayan fonksiyon kodu
